e47baa880faf81da56cbd6d36be5709af179ac7c
Njdonatelli <114975076+Njdonatelli@users.noreply.github.com>
2026-01-06 14:56:09 -0800

    Delete mythic-bastionland-fvtt-main2/mythic-bastionland-fvtt-main directory

--- mythic-bastionland-fvtt-main2/mythic-bastionland-fvtt-main/i18n-unused.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  files: ['./lang/*.json'],
-  localesPath: './lang',
-  srcPath: '.',
-  srcExtensions: ['js', 'hbs'],
-  translationKeyMatcher: /(localize "|localize\("|format\(")(.*?)("|"\))/ig,
-};
